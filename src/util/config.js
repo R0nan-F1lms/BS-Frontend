38216b94b8f39a8efad7650ceecb1e5e648dcf1f
Ronan Films <88947716+R0nan-F1lms@users.noreply.github.com>
2023-11-02 07:53:31 +1100

    2nd nov commit

--- src/util/config.js
+++ src/util/config.js
@@ -2,5 +2,5 @@
 export let api = 'http://192.168.1.143';
 export const config = {
   //BASE_URL: `${api}:5000/api`, 
-  BASE_URL: 'https://yourappname.herokuapp.com/api'
+  BASE_URL: 'https://beanscene-food-app-22de0f83a329.herokuapp.com/api'
 };
